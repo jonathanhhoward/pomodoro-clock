b01b937645beb1dc04e0c4f544719c2d25e6a198
slipskid <2773262@gmail.com>
2019-10-03 14:33:05 -0400

    Add state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 
 export default function App () {
+  const initialState = {
+    breakLength: 5,
+    sessionLength: 25,
+    timerLabel: 'Session',
+    timerLength: '25:00',
+    timerButton: 'Start',
+  }
+  const [state, setState] = useState(initialState)
   return (
     <div>
       <h1>Pomodoro Clock</h1>
       <div>
         <h2>Break Length</h2>
-        <div>5</div>
+        <div>{state.breakLength}</div>
         <button>-</button>
         <button>+</button>
       </div>
       <div>
         <h2>Session Length</h2>
-        <div>25</div>
+        <div>{state.sessionLength}</div>
         <button>-</button>
         <button>+</button>
       </div>
       <div>
-        <h2>Session</h2>
-        <div>25:00</div>
-        <button>Start/Stop</button>
+        <h2>{state.timerLabel}</h2>
+        <div>{state.timerLength}</div>
+        <button>{state.timerButton}</button>
         <button>Reset</button>
       </div>
     </div>
