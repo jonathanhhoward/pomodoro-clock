78e480fd923af93691203f009baf20cb97024528
slipskid <2773262@gmail.com>
2019-10-03 14:09:28 -0400

    Add basic structure and layout

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from 'react'
 import './App.css'
 
 export default function App () {
-  return <div/>
+  return (
+    <div>
+      <h1>Pomodoro Clock</h1>
+      <div>
+        <h2>Break Length</h2>
+        <div>5</div>
+        <button>-</button>
+        <button>+</button>
+      </div>
+      <div>
+        <h2>Session Length</h2>
+        <div>25</div>
+        <button>-</button>
+        <button>+</button>
+      </div>
+      <div>
+        <h2>Session</h2>
+        <div>25:00</div>
+        <button>Start/Stop</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  )
 }
