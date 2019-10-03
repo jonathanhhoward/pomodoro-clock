b28b91587a4a0bb8c6050989b54fb1a2ce515447
slipskid <2773262@gmail.com>
2019-10-03 15:39:36 -0400

    Add onClick functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 export default function App () {
@@ -10,26 +10,79 @@ export default function App () {
     timerButton: 'Start',
   }
   const [state, setState] = useState(initialState)
+  const handleDecrement = (event) => {
+    switch (event.target.value) {
+      case 'break':
+        if (state.breakLength === 1) return
+        setState(prev => ({
+          ...prev,
+          breakLength: prev.breakLength - 1
+        }))
+        break
+      case 'session':
+        if (state.sessionLength === 1) return
+        setState(prev => ({
+          ...prev,
+          sessionLength: prev.sessionLength - 1
+        }))
+        break
+      default:
+    }
+  }
+  const handleIncrement = (event) => {
+    switch (event.target.value) {
+      case 'break':
+        if (state.breakLength === 60) return
+        setState(prev => ({
+          ...prev,
+          breakLength: prev.breakLength + 1
+        }))
+        break
+      case 'session':
+        if (state.sessionLength === 60) return
+        setState(prev => ({
+          ...prev,
+          sessionLength: prev.sessionLength + 1
+        }))
+        break
+      default:
+    }
+  }
+  const handleStartStop = () => {
+    setState(prev => ({
+      ...prev,
+      timerButton: prev.timerButton === 'Start' ? 'Stop' : 'Start',
+    }))
+    /**
+     * Todo: (set/clear)Interval
+     */
+  }
+  const handleReset = () => {
+    setState(initialState)
+    /**
+     * Todo: clearInterval, stop audio
+     */
+  }
   return (
     <div>
       <h1>Pomodoro Clock</h1>
       <div>
         <h2>Break Length</h2>
         <div>{state.breakLength}</div>
-        <button>-</button>
-        <button>+</button>
+        <button onClick={handleDecrement} value={'break'}>-</button>
+        <button onClick={handleIncrement} value={'break'}>+</button>
       </div>
       <div>
         <h2>Session Length</h2>
         <div>{state.sessionLength}</div>
-        <button>-</button>
-        <button>+</button>
+        <button onClick={handleDecrement} value={'session'}>-</button>
+        <button onClick={handleIncrement} value={'session'}>+</button>
       </div>
       <div>
         <h2>{state.timerLabel}</h2>
         <div>{state.timerLength}</div>
-        <button>{state.timerButton}</button>
-        <button>Reset</button>
+        <button onClick={handleStartStop}>{state.timerButton}</button>
+        <button onClick={handleReset}>Reset</button>
       </div>
     </div>
   )
