7934f0845afe0b54ce6a6158dfc72ee3dc9571ff
slipskid <2773262@gmail.com>
2019-10-04 00:05:19 -0400

    Add countdown timer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 export default function App () {
@@ -6,83 +6,105 @@ export default function App () {
     breakLength: 5,
     sessionLength: 25,
     timerLabel: 'Session',
-    timerLength: '25:00',
-    timerButton: 'Start',
+    timeLeft: 1500,
+    startStop: 'Start',
   }
+
   const [state, setState] = useState(initialState)
+
+  useEffect(() => {
+    let timer = null
+    if (state.startStop === 'Stop') {
+      timer = setInterval(() => {
+        setState(state => ({
+          ...state,
+          timeLeft: state.timeLeft - 1
+        }))
+      }, 1000)
+    } else {
+      clearInterval(timer)
+    }
+    return () => clearInterval(timer)
+  }, [state.startStop])
+
   const handleDecrement = (event) => {
-    switch (event.target.value) {
+    const type = event.target.id.match(/break|session/)[0]
+    switch (type) {
       case 'break':
         if (state.breakLength === 1) return
-        setState(prev => ({
-          ...prev,
-          breakLength: prev.breakLength - 1
+        setState(state => ({
+          ...state,
+          breakLength: state.breakLength - 1,
         }))
         break
       case 'session':
         if (state.sessionLength === 1) return
         setState(prev => ({
           ...prev,
-          sessionLength: prev.sessionLength - 1
+          sessionLength: prev.sessionLength - 1,
         }))
         break
       default:
     }
   }
+
   const handleIncrement = (event) => {
-    switch (event.target.value) {
+    const type = event.target.id.match(/break|session/)[0]
+    switch (type) {
       case 'break':
         if (state.breakLength === 60) return
-        setState(prev => ({
-          ...prev,
-          breakLength: prev.breakLength + 1
+        setState(state => ({
+          ...state,
+          breakLength: state.breakLength + 1,
         }))
         break
       case 'session':
         if (state.sessionLength === 60) return
-        setState(prev => ({
-          ...prev,
-          sessionLength: prev.sessionLength + 1
+        setState(state => ({
+          ...state,
+          sessionLength: state.sessionLength + 1,
         }))
         break
       default:
     }
   }
+
   const handleStartStop = () => {
-    setState(prev => ({
-      ...prev,
-      timerButton: prev.timerButton === 'Start' ? 'Stop' : 'Start',
+    setState(state => ({
+      ...state,
+      startStop: state.startStop === 'Start' ? 'Stop' : 'Start',
     }))
-    /**
-     * Todo: (set/clear)Interval
-     */
   }
+
   const handleReset = () => {
     setState(initialState)
     /**
-     * Todo: clearInterval, stop audio
+     * Todo: stop audio
      */
   }
+
   return (
     <div>
       <h1>Pomodoro Clock</h1>
       <div>
-        <h2>Break Length</h2>
-        <div>{state.breakLength}</div>
-        <button onClick={handleDecrement} value={'break'}>-</button>
-        <button onClick={handleIncrement} value={'break'}>+</button>
+        <h2 id={'break-label'}>Break Length</h2>
+        <div id={'break-length'}>{state.breakLength}</div>
+        <button id={'break-decrement'} onClick={handleDecrement}>-</button>
+        <button id={'break-increment'} onClick={handleIncrement}>+</button>
       </div>
       <div>
-        <h2>Session Length</h2>
-        <div>{state.sessionLength}</div>
-        <button onClick={handleDecrement} value={'session'}>-</button>
-        <button onClick={handleIncrement} value={'session'}>+</button>
+        <h2 id={'session-label'}>Session Length</h2>
+        <div id={'session-length'}>{state.sessionLength}</div>
+        <button id={'session-decrement'} onClick={handleDecrement}>-</button>
+        <button id={'session-increment'} onClick={handleIncrement}>+</button>
       </div>
       <div>
-        <h2>{state.timerLabel}</h2>
-        <div>{state.timerLength}</div>
-        <button onClick={handleStartStop}>{state.timerButton}</button>
-        <button onClick={handleReset}>Reset</button>
+        <h2 id={'timer-label'}>{state.timerLabel}</h2>
+        <div id={'time-left'}>{state.timeLeft}</div>
+        <button id={'start_stop'} onClick={handleStartStop}>
+          {state.startStop}
+        </button>
+        <button id={'reset'} onClick={handleReset}>Reset</button>
       </div>
     </div>
   )
