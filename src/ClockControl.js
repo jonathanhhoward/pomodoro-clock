38eea6f57c91348100353554641ff7700952557f
slipskid <2773262@gmail.com>
2019-10-11 12:59:35 -0400

    Add component

---
+++ src/ClockControl.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function ClockControl (props) {
+  const {h2, div, button1, button2} = props.data
+  return (
+    <div id={props.name} className={'ClockControl ' + props.class}>
+      <h2 id={h2.id} className={h2.class}>
+        {h2.text}
+      </h2>
+      <div id={div.id} className={div.class}>
+        {div.text}
+      </div>
+      <button
+        id={button1.id}
+        className={button1.class}
+        onClick={button1.callback}
+      >
+        {button1.text}
+      </button>
+      <button
+        id={button2.id}
+        className={button2.class}
+        onClick={button2.callback}
+      >
+        {button2.text}
+      </button>
+    </div>
+  )
+}
