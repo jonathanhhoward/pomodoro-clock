c939a335fe85426ddc5f9f342a8b1b7d53497203
slipskid <2773262@gmail.com>
2019-10-13 18:57:14 -0400

    Updates

---
+++ precache-manifest.bf7d6f2d83642d579a29dbfe2eaf11ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc6ced782326cbdf3741f17a19daf67e",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "7917fd18d42095c8d96b",
+    "url": "/pomodoro-clock/static/css/main.554113e5.chunk.css"
+  },
+  {
+    "revision": "23810e3b7186df774b79",
+    "url": "/pomodoro-clock/static/js/2.0934ff13.chunk.js"
+  },
+  {
+    "revision": "7917fd18d42095c8d96b",
+    "url": "/pomodoro-clock/static/js/main.8c47f775.chunk.js"
+  },
+  {
+    "revision": "18b2b2f06e6d6ef31530",
+    "url": "/pomodoro-clock/static/js/runtime-main.cc612b39.js"
+  }
+]);
