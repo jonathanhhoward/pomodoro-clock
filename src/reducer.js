07d930bdff69656a7637d5509fdc293b03352e2b
jonathanhhoward <2773262@gmail.com>
2020-09-09 16:46:56 -0500

    Inline constants

--- src/reducer.js
+++ src/reducer.js
@@ -1,19 +1,17 @@
 function reducer (state, action) {
-  const SEC_PER_MIN = 60
-
   switch (action.type) {
     case 'break-decrement':
       return { ...state, breakLength: state.breakLength - 1 }
     case 'break-increment':
       return { ...state, breakLength: state.breakLength + 1 }
     case 'update-break':
-      return { ...state, timeLeft: state.breakLength * SEC_PER_MIN }
+      return { ...state, timeLeft: state.breakLength * 60 }
     case 'session-decrement':
       return { ...state, sessionLength: state.sessionLength - 1 }
     case 'session-increment':
       return { ...state, sessionLength: state.sessionLength + 1 }
     case 'update-session':
-      return { ...state, timeLeft: state.sessionLength * SEC_PER_MIN }
+      return { ...state, timeLeft: state.sessionLength * 60 }
     case 'toggle-startStop':
       return { ...state, startStop: state.startStop === 'START' ? 'STOP' : 'START' }
     case 'countdown':
@@ -22,13 +20,13 @@ function reducer (state, action) {
       return {
         ...state,
         timerLabel: 'Break',
-        timeLeft: state.breakLength * SEC_PER_MIN,
+        timeLeft: state.breakLength * 60,
       }
     case 'toggle-session':
       return {
         ...state,
         timerLabel: 'Session',
-        timeLeft: state.sessionLength * SEC_PER_MIN,
+        timeLeft: state.sessionLength * 60,
       }
     case 'reset':
       return action.payload
