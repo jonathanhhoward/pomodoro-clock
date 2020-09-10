785ffac98952c8aa6010cf3c160413a579c198b1
jonathanhhoward <2773262@gmail.com>
2020-09-09 20:57:34 -0500

    Refactor reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,37 +1,71 @@
 function reducer (state, action) {
+  if (action.type === 'reset') {
+    return action.payload
+  }
+
+  return {
+    sessionLength: sessionLengthReducer(state.sessionLength, action),
+    breakLength: breakLengthReducer(state.breakLength, action),
+    timerLabel: timerLabelReducer(state.timerLabel, action),
+    startStop: startStopReducer(state.startStop, action),
+    timeLeft: timeLeftReducer(state, action)
+  }
+}
+
+function sessionLengthReducer (sessionLength, action) {
+  switch (action.type) {
+    case 'session-decrement':
+      return sessionLength - 1
+    case 'session-increment':
+      return sessionLength + 1
+    default:
+      return sessionLength
+  }
+}
+
+function breakLengthReducer (breakLength, action) {
   switch (action.type) {
     case 'break-decrement':
-      return { ...state, breakLength: state.breakLength - 1 }
+      return breakLength - 1
     case 'break-increment':
-      return { ...state, breakLength: state.breakLength + 1 }
-    case 'update-break':
-      return { ...state, timeLeft: state.breakLength * 60 }
-    case 'session-decrement':
-      return { ...state, sessionLength: state.sessionLength - 1 }
-    case 'session-increment':
-      return { ...state, sessionLength: state.sessionLength + 1 }
-    case 'update-session':
-      return { ...state, timeLeft: state.sessionLength * 60 }
-    case 'toggle-startStop':
-      return { ...state, startStop: state.startStop === 'START' ? 'STOP' : 'START' }
-    case 'countdown':
-      return { ...state, timeLeft: state.timeLeft - 1 }
-    case 'toggle-break':
-      return {
-        ...state,
-        timerLabel: 'Break',
-        timeLeft: state.breakLength * 60
-      }
-    case 'toggle-session':
-      return {
-        ...state,
-        timerLabel: 'Session',
-        timeLeft: state.sessionLength * 60
-      }
-    case 'reset':
-      return action.payload
+      return breakLength + 1
     default:
-      return state
+      return breakLength
+  }
+}
+
+function timerLabelReducer (timerLabel, action) {
+  switch (action.type) {
+    case 'toggle-session':
+      return 'Session'
+    case 'toggle-break':
+      return 'Break'
+    default:
+      return timerLabel
+  }
+}
+
+function startStopReducer (startStop, action) {
+  switch (action.type) {
+    case 'toggle-startStop':
+      return (startStop === 'START') ? 'STOP' : 'START'
+    default:
+      return startStop
+  }
+}
+
+function timeLeftReducer (state, action) {
+  switch (action.type) {
+    case 'update-session':
+    case 'toggle-session':
+      return state.sessionLength * 60
+    case 'update-break':
+    case 'toggle-break':
+      return state.breakLength * 60
+    case 'countdown':
+      return state.timeLeft - 1
+    default:
+      return state.timeLeft
   }
 }
 
