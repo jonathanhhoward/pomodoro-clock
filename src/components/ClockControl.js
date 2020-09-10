a75840ccf84817c87c05720d06305e3e2667f300
jonathanhhoward <2773262@gmail.com>
2020-09-10 18:50:11 -0500

    Refactor props

--- src/components/ClockControl.js
+++ src/components/ClockControl.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function ClockControl (props) {
-  const { h2, div, button1, button2 } = props.data
+function ClockControl ({ data }) {
+  const { h2, div, button1, button2 } = data
   return (
     <div className="ClockControl">
       <h2 id={h2.id} className={h2.class}>
