3078116f00bc6b0364ad29b492cc0d92eec99c0d
jonathanhhoward <2773262@gmail.com>
2020-09-21 19:21:53 -0400

    Move countdown timer, timer label toggler, handleStartStop, and handleReset to TimerControl

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import BreakControl from './components/BreakControl';
 import SessionControl from './components/SessionControl';
 import TimerControl from './components/TimerControl';
@@ -17,41 +17,6 @@ function App() {
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  useEffect(() => {
-    let timer = null;
-
-    if (state.startStop === 'STOP') {
-      timer = setInterval(() => {
-        dispatch({ type: 'countdown' });
-      }, 1000);
-    } else {
-      clearInterval(timer);
-    }
-
-    return () => clearInterval(timer);
-  }, [state.startStop]);
-
-  useEffect(() => {
-    if (state.timeLeft !== 0) return;
-
-    if (state.timerLabel === 'Session') {
-      dispatch({ type: 'toggle-break' });
-    } else {
-      dispatch({ type: 'toggle-session' });
-    }
-
-    document.getElementById('beep').play();
-  }, [state.timerLabel, state.timeLeft]);
-
-  function handleStartStop() {
-    dispatch({ type: 'toggle-startStop' });
-  }
-
-  function handleReset() {
-    dispatch({ type: 'reset', payload: initialState });
-    document.getElementById('beep').load();
-  }
-
   return (
     <div className="clock">
       <h1>Pomodoro Clock</h1>
@@ -60,11 +25,9 @@ function App() {
         <SessionControl state={state} dispatch={dispatch} />
       </div>
       <TimerControl
-        timerLabel={state.timerLabel}
-        timeLeft={state.timeLeft}
-        startStop={state.startStop}
-        onClickStartStop={handleStartStop}
-        onClickReset={handleReset}
+        state={state}
+        dispatch={dispatch}
+        initialState={initialState}
       />
       <audio id="beep" src={beepSound} preload="auto" />
     </div>
--- src/components/TimerControl.js
+++ src/components/TimerControl.js
@@ -1,35 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ClockControl from './ClockControl';
 import formatSecondsAsMMSS from '../formatSecondsAsMMSS';
 
-function TimerControl({
-  timerLabel,
-  timeLeft,
-  startStop,
-  onClickStartStop,
-  onClickReset,
-}) {
+function TimerControl({ state, dispatch, initialState }) {
+  useEffect(() => {
+    let timer = null;
+
+    if (state.startStop === 'STOP') {
+      timer = setInterval(() => {
+        dispatch({ type: 'countdown' });
+      }, 1000);
+    } else {
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer);
+  }, [state.startStop]);
+
+  useEffect(() => {
+    if (state.timeLeft !== 0) return;
+
+    if (state.timerLabel === 'Session') {
+      dispatch({ type: 'toggle-break' });
+    } else {
+      dispatch({ type: 'toggle-session' });
+    }
+
+    document.getElementById('beep').play();
+  }, [state.timerLabel, state.timeLeft]);
+
+  function handleStartStop() {
+    dispatch({ type: 'toggle-startStop' });
+  }
+
+  function handleReset() {
+    dispatch({ type: 'reset', payload: initialState });
+    document.getElementById('beep').load();
+  }
+
   const timerData = {
     h2: {
       id: 'timer-label',
       class: null,
-      text: timerLabel,
+      text: state.timerLabel,
     },
     div: {
       id: 'time-left',
       class: 'time',
-      text: formatSecondsAsMMSS(timeLeft),
+      text: formatSecondsAsMMSS(state.timeLeft),
     },
     button1: {
       id: 'start_stop',
       class: 'pill',
-      callback: onClickStartStop,
-      text: startStop,
+      callback: handleStartStop,
+      text: state.startStop,
     },
     button2: {
       id: 'reset',
       class: 'pill',
-      callback: onClickReset,
+      callback: handleReset,
       text: 'RESET',
     },
   };
